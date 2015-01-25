63964d89d149762b11c163e5db651d8f9e957c21
Mustafa, KARA <karamusti@gmail.com>
2015-01-25 18:00:56 +0200

    Test 6

--- server.js
+++ server.js
@@ -1,52 +1,14 @@
    var redis = require("redis"),
-        client = redis.createClient();
+       sample = require('sample'),
+       client = redis.createClient();
 
     client.on("error", function (err) {
         console.log("Error " + err);
     });
-    
 
-    var Device001 = {
-       lastSentence: '$KIPYO,Device001,XXXX1',
-       logs: [
-            '$KIPYO,Device001,XXXX1',
-            '$KIPYO,Device001,XXXX2',
-            '$KIPYO,Device001,XXXX3',
-            '$KIPYO,Device001,XXXX4',
-            '$KIPYO,Device001,XXXX5'
-        ]
-    };
-
-    var Device002 = {
-       lastSentence: '$KIPYO,Device002,YYYY1',
-       logs: [
-            '$KIPYO,Device002,YYYY1',
-            '$KIPYO,Device002,YYYY2',
-            '$KIPYO,Device002,YYYY3',
-            '$KIPYO,Device002,YYYY4',
-            '$KIPYO,Device002,YYYY5'
-        ]
-    };
-    
-    
-    client.hget('realtime', 'Device003', function(err, result){
-        if(err) throw err;
-        console.log(result);
-    });
-
-    function tracking(id, sentence, cb){
-        client.hget(id, function(err, result){
-            if(err) throw err;
-            
-            var device = JSON.parse(result);
-            device.logs.push(sentence);
-            
-            client.hset('realtime', id,  JSON.stringify(device), redis.print);
-        });
+    var i = 0;
+    function dummySentence(id){
+        return '$KIPYO,' + id + ',SENTENCE' + i;
     }
 
- /*setTimeout(function(){
-        tracking('Device001', '$KIPYO,Device001,XXXX1');
-    
-    }, 5000);*/
-
+    sample.init();
